2cb8d10bf191cb0e5e068626cbd66557da1e4fa6
KirillFave <youngfave@yandex.ru>
2026-04-11 23:32:12 +0700

    fix [Wheel]: list init

--- Domain/Wheel/WheelList.cs
+++ Domain/Wheel/WheelList.cs
@@ -5,5 +5,5 @@ public class WheelList
     public Guid Guid { get; set; } = Guid.NewGuid();
     public bool IsDeleted { get; set; }
     public required string Name { get; set; }
-    public virtual ICollection<WheelItem>? Items { get; set; }
+    public virtual ICollection<WheelItem> Items { get; set; } = [];
 }
