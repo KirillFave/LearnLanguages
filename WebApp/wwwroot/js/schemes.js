56f1676591c661cded59b413e5eac25f4e2ce38c
KirillFave <youngfave@yandex.ru>
2026-04-21 00:27:46 +0700

    Начало работы над _AddSchemeItemPopover

--- WebApp/wwwroot/js/schemes.js
+++ WebApp/wwwroot/js/schemes.js
@@ -1,1 +1,15 @@
-﻿
+﻿document.addEventListener('dblclick', e => {
+    if (e.target.classList.contains('scheme-item')) {
+        const schemeItem = e.target;
+        const row = schemeItem.dataset.row;
+        const column = schemeItem.dataset.column;
+
+        const addSchemeItemPopover = document.getElementById('add-scheme-item-popover');
+
+        addSchemeItemPopover.classList.remove('d-none');
+    }
+});
+
+document.getElementById('confirm-add-item-btn').addEventListener('click', e => {
+    text = 
+});
