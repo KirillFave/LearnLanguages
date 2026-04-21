326ebe59e80ee8b3de612bb2a59c5037cef060e2
KirillFave <youngfave@yandex.ru>
2026-04-22 00:49:52 +0700

    позиционирование _AddSchemeItemPopover

--- WebApp/wwwroot/js/schemes.js
+++ WebApp/wwwroot/js/schemes.js
@@ -5,11 +5,31 @@
         const column = schemeItem.dataset.column;
 
         const addSchemeItemPopover = document.getElementById('add-scheme-item-popover');
+        const popoverHeight = addSchemeItemPopover.offsetHeight;
+        const popoverWidth = addSchemeItemPopover.offsetWidth;
+
+        const rect = schemeItem.getBoundingClientRect();
+
+        let top = rect.top;
+
+        if (top + popoverHeight > window.innerHeight) {
+            top = window.innerHeight - popoverHeight;
+        }
+
+        let left = rect.right;
+
+        if (left + popoverWidth > window.innerWidth) {
+            left = rect.left - popoverWidth;
+        }
+
+        addSchemeItemPopover.style.top = top + 'px';
+        addSchemeItemPopover.style.left = left + 'px';
+        
 
         addSchemeItemPopover.classList.remove('d-none');
     }
 });
 
 document.getElementById('confirm-add-item-btn').addEventListener('click', e => {
-    text = 
+    console.log();
 });
