74ca7915664f082729c2bb5654c6400037aa3f87
KirillFave <youngfave@yandex.ru>
2026-04-23 00:17:11 +0700

    migration

--- DataAccess/Migrations/20260422171040_Scheme_SchemeItem_Entities_Init.Designer.cs
+++ DataAccess/Migrations/20260422171040_Scheme_SchemeItem_Entities_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20260422171040_Scheme_SchemeItem_Entities_Init")]
+    partial class Scheme_SchemeItem_Entities_Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DataAccess/Migrations/20260422171040_Scheme_SchemeItem_Entities_Init.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Scheme_SchemeItem_Entities_Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Schemes",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schemes", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SchemeItems",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false),
+                    Column = table.Column<int>(type: "INTEGER", nullable: false),
+                    Row = table.Column<int>(type: "INTEGER", nullable: false),
+                    SchemeGuid = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchemeItems", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_SchemeItems_Schemes_SchemeGuid",
+                        column: x => x.SchemeGuid,
+                        principalTable: "Schemes",
+                        principalColumn: "Guid");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SchemeItems_SchemeGuid",
+                table: "SchemeItems",
+                column: "SchemeGuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SchemeItems");
+
+            migrationBuilder.DropTable(
+                name: "Schemes");
+        }
+    }
+}
